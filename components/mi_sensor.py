84bab3c67eed12f1fb614bd05ddb2ca61a27cc8c
haiyang.song <songhaiyang@shuwen.com>
2019-07-29 21:39:46 +0800

    init project

--- components/mi_sensor.py
+++ components/mi_sensor.py
@@ -33,7 +33,6 @@ class MiSensor:
         mac = self._poller._mac
         try:
             self._poller.fill_cache()
-            self._poller.parameter_value(MI_LIGHT)
             firmware = self._poller.firmware_version()
         except (IOError, BluetoothBackendException):
             print('Initial connection to Mi Flora sensor "{}" ({}) failed.'.format(name, mac))
